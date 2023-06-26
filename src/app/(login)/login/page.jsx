322ce43ad0653eee623990c2cba6431ada122d55
SuyashPatil-29 <112413583+SuyashPatil-29@users.noreply.github.com>
2023-06-26 15:50:25 +0530

    Finished the login page

--- src/app/(login)/login/page.jsx
+++ src/app/(login)/login/page.jsx
@@ -19,15 +19,14 @@ const Login = () => {
 
 
     return (
-        <div className="h-[60vh] grid place-content-center bg-black rounded-2xl mt-20 relative">
-      <div className='flex flex-col justify-center items-center gap-7'>
-      <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
-          Login
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
-            To Continue
-          </span>
-          Shopping
+        <div className=" h-[80vh] grid bg-black relative">
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+            WELCOME TO <span className="font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">SpenseStore</span> 
         </h1>
+          <p className=" text-[rgb(124,134,227)] leading-normal text-base md:text-2xl mb-8 text-center md:text-left">
+            Login to your account to continue or continue as a guest.
+          </p>
         <button
           className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out max-w-[200px]"
           type="button"
@@ -35,7 +34,7 @@ const Login = () => {
         >
           Log in using Google
         </button>
-        <Link href="/admin/desk/vendor" className='text-pink-700 underline absolute bottom-4'>Log In As Vendor/Admin</Link>
+        <Link href="/admin/desk/vendor" className='text-pink-700 underline absolute bottom-2'>Log In As Vendor/Admin</Link>
       </div>
       </div>
     );
