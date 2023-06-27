84556f9df423bdbe0683e6a86ac18fad261a2280
SuyashPatil-29 <112413583+SuyashPatil-29@users.noreply.github.com>
2023-06-27 22:48:05 +0530

    Finished the buy now button

--- src/app/(checkouts)/success/page.jsx
+++ src/app/(checkouts)/success/page.jsx
@@ -1,13 +1,14 @@
 "use client"
-import { useRouter } from "next/navigation";
 import React, { useEffect } from 'react';
+import Link from 'next/link';
+import { BsBagCheckFill } from 'react-icons/bs';
 import { toast } from "react-hot-toast";
+import {runFireworks} from "../../../../LIB/fireworks.js"
 
-const PaymentsSuccess = () => {
-  const {push} = useRouter();
-
+const Success = () => {
   useEffect(() => {
-    const toastId = toast.success((`Payment successful!`));
+    runFireworks();
+    const toastId = toast.success("Payment successful");
 
     const timeout = setTimeout(() => {
       toast.dismiss(toastId);
@@ -16,23 +17,33 @@ const PaymentsSuccess = () => {
     return () => {
       clearTimeout(timeout);
       toast.dismiss(toastId);
-    };
+    }
   }, []);
 
   const handleContinueShopping = () => {
     localStorage.clear(); // Clear browser local storage data
-    push("/"); // Navigate to home page
   };
 
   return (
-    <div className='h-screen w-screen flex justify-center items-center bg-black'>
-      <div className='flex flex-col justify-center items-center'>
-        <h1 className='my-4 text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left'>Thank you for your purchase.</h1>
-        <button onClick={handleContinueShopping} className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out mt-6 min-w-[250px] text-center">Continue Shopping</button>
+    <div className="flex justify-center items-center h-screen bg-black">
+      <div className="success">
+        <p className="icon text-green-500">
+          <BsBagCheckFill />
+        </p>
+        <h2 className='text-white'>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:patilsuyash892@gmail.com">
+            patilsuyash892@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+            <button onClick={handleContinueShopping} className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-4 px-4 rounded-xl focus:ring transform transition hover:scale-105 duration-300 ease-in-out mt-6 min-w-[250px] text-center">Continue Shopping</button>
+        </Link>
       </div>
     </div>
-  );
+  )
 }
 
-export default PaymentsSuccess;
-
+export default Success
